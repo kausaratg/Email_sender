fa6100ca491ef776c818c74512ab276d95cca7ae
kassy <kausaratg@gmail.com>
2022-09-10 12:33:54 +0100

    added email address

--- email_sender/views.py
+++ email_sender/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from email_sender.models import SendEmail
 from email_sender.forms import SendEmailForm
 from django.contrib import messages
+from django.conf import settings
+from django.core.mail import send_mail
 # Create your views here.
 
 def send(request):
@@ -9,11 +11,16 @@ def send(request):
         form = SendEmailForm(request.POST)
         if form.is_valid():
             form.save()
+            subject = "message"
+            email = form.cleaned_data.get('email')
+            message = form.cleaned_data.get('message')
+            email_from = settings.EMAIL_HOST_USER
+            recipient_list = [email,]
+            send_mail( subject, message, email_from, recipient_list )
             return redirect('email_confirm')
         else:
             messages.info(request, "Invalid input")
             return redirect('send')
-        
     form = SendEmailForm()
     context = {'form':form}
     return render(request, 'email_templates.html', context)
--- kassy_email/settings.py
+++ kassy_email/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-zvrihzdg(4hv_9-t1)f5qn0!jeltihl$zor(n@j1kc#&n^o1kz"
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -121,6 +122,13 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 MEDIA_URL  = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_USER = 'authwiki5@gmail.com'
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
